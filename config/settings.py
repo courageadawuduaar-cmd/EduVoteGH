5b2f9d0f34b90622507abace88d39c7f70b25c41
Dacosta <324services.cls@gmail.com>
2026-02-18 18:26:11 +0000

    Fixed Cloudinary configuration

--- config/settings.py
+++ config/settings.py
@@ -7,10 +7,6 @@ from pathlib import Path
 import os
 import dj_database_url
 
-os.environ['CLOUDINARY_CLOUD_NAME'] = 'your_cloud_name'
-os.environ['CLOUDINARY_API_KEY'] = 'your_api_key'
-os.environ['CLOUDINARY_API_SECRET'] = 'your_api_secret'
-
 # -------------------------------------------------
 # Base Directory
 # -------------------------------------------------
@@ -185,11 +181,10 @@ DEFAULT_FROM_EMAIL = 'admin@electionsystem.com'
 
 import cloudinary
 
-if RENDER:
-    CLOUDINARY_STORAGE = {
-        'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
-        'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
-        'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
-    }
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+}
 
-    DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
