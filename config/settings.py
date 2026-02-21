9cb7fadf683e7f23c3fb61928918d3d838c243bd
Dacosta <324services.cls@gmail.com>
2026-02-21 10:31:00 +0000

    Fix Cloudinary storage configuration

--- config/settings.py
+++ config/settings.py
@@ -158,3 +158,4 @@ if not DEBUG:
         'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
         'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
         'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+    }
