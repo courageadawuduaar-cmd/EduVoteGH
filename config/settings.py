37db6beb9f0a26ad6fafbee5647b37f9fef1e8c2
Dacosta <324services.cls@gmail.com>
2026-02-21 07:15:45 +0000

    Use environment variables for DEBUG and SECRET_KEY

--- config/settings.py
+++ config/settings.py
@@ -15,9 +15,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # -------------------------------------------------
 # Security & Environment Settings (LOCAL)
 # -------------------------------------------------
-SECRET_KEY = "dev-secret-key"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
