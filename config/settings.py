580ca9858d51df18d46f1f41e8061a649dcda864
Dacosta <324services.cls@gmail.com>
2026-02-22 16:07:23 +0000

    Force Cloudinary as default storage (remove conditional block)

--- config/settings.py
+++ config/settings.py
@@ -149,13 +149,10 @@ INSTALLED_APPS += [
     'cloudinary_storage',
 ]
 
-if os.environ.get("CLOUDINARY_CLOUD_NAME"):
-    DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
-    CLOUDINARY_STORAGE = {
-        'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
-        'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
-        'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
-    }
-    
-    print("DEFAULT STORAGE:", DEFAULT_FILE_STORAGE) 
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+}
